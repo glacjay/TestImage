01eb5f8025c596b583c5b1f3aee7772cd3558a11
GlacJAY <glacjay@gmail.com>
2019-07-17 20:37:43 +0800

    update to rn 0.60.3

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component {
               "https://wagonsclub.oss-cn-beijing.aliyuncs.com/static/carousel/carousel1_bg.jpg"
           }}
           style={{ width: Dimensions.get("window").width, height: 1000 }}
+          resizeMode="cover"
         />
       </View>
     );
